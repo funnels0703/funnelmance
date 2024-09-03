aea7fd8860052be56668bc4e022e19ee4166481b
LLLL <funnels0703@gmail.com>
2024-09-04 08:49:07 +0900

    됐다

--- back-end/src/routes/listRoutes.js
+++ back-end/src/routes/listRoutes.js
@@ -1,29 +1,29 @@
 // routes/urlCodeRoutes.js
-const express = require("express");
+const express = require('express');
 const {
-  getHospitals,
-  addHospital,
-  getEvents,
-  addEvent,
-  getAdvertisingCompanies,
-  addAdvertisingCompany,
-  updateHospital,
-  updateEvent,
-  updateAdvertisingCompany,
-} = require("../modules/listmodule");
+    getHospitals,
+    addHospital,
+    getEvents,
+    addEvent,
+    getAdvertisingCompanies,
+    addAdvertisingCompany,
+    updateHospital,
+    updateEvent,
+    updateAdvertisingCompany,
+} = require('../controllers/listControllers');
 
 const router = express.Router();
 
-router.get("/hospitals", getHospitals);
-router.post("/hospitals", addHospital);
-router.put("/hospitals/:id", updateHospital); // 병원 정보 수정
+router.get('/hospitals', getHospitals);
+router.post('/hospitals', addHospital);
+router.put('/hospitals/:id', updateHospital); // 병원 정보 수정
 
-router.get("/events", getEvents);
-router.post("/events", addEvent);
-router.put("/events/:id", updateEvent); // 이벤트 정보 수정
+router.get('/events', getEvents);
+router.post('/events', addEvent);
+router.put('/events/:id', updateEvent); // 이벤트 정보 수정
 
-router.get("/advertising_companies", getAdvertisingCompanies);
-router.post("/advertising_companies", addAdvertisingCompany);
-router.put("/advertising_companies/:id", updateAdvertisingCompany); // 매체 정보 수정
+router.get('/advertising_companies', getAdvertisingCompanies);
+router.post('/advertising_companies', addAdvertisingCompany);
+router.put('/advertising_companies/:id', updateAdvertisingCompany); // 매체 정보 수정
 
 module.exports = router;
