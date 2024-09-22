0c12cb8e4efa4dac33aee340431c4216d86dc571
funnelsJ <lj_yeon_@naver.com>
2024-09-23 00:26:03 +0900

    사이드 메뉴 수정

--- back-end/front-end/src/layout/SideMenu.jsx
+++ back-end/front-end/src/layout/SideMenu.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function SideMenu() {
   const [openIndexes, setOpenIndexes] = useState({});
+  const location = useLocation();
 
   const toggleSubMenu = (index) => {
     setOpenIndexes((prevState) => ({
@@ -18,18 +19,18 @@ function SideMenu() {
       title: "광고비입력",
       path: "/ad-cost",
       subMenu: [
-        { title: "광고비입력", path: "/" },
-        { title: "히스토리", path: "/" },
+        { title: "광고비입력", path: "/ad-cost" },
+        { title: "히스토리", path: "/ad-cost/2" },
       ],
     },
-    { title: "성과 리포트", path: "/" },
+    { title: "성과 리포트", path: "/dd" },
     {
       title: "TM",
       path: "/tm",
       subMenu: [
         { title: "담당자 업무 현황", path: "/tm" },
-        { title: "방문 데이터", path: "/" },
-        { title: "예약달력", path: "/" },
+        { title: "방문 데이터", path: "/tm/1" },
+        { title: "예약달력", path: "/tm/2" },
         { title: "공지", path: "/tm/notices" },
       ],
     },
@@ -42,32 +43,61 @@ function SideMenu() {
       path: "/admin",
       subMenu: [
         { title: "병원/이벤트/매체 리스트", path: "/admin/listsetting" },
-        { title: "지점별 이벤트", path: "/" },
+        { title: "지점별 이벤트", path: "/admin/1" },
         { title: "계정 관리", path: "/admin/UserManagement" },
       ],
     },
     { title: "휴지통", path: "/trashCanData" },
   ];
 
+  const isMainMenuActive = (menu) => {
+    return (
+      location.pathname === menu.path ||
+      (menu.subMenu &&
+        menu.subMenu.some((subItem) => location.pathname === subItem.path))
+    );
+  };
+
   return (
     <nav>
       <ul className="main_menu">
-        {menuItems.map((item, index) => (
-          <li key={index}>
-            <Link to={item.path} onClick={() => toggleSubMenu(index)}>
-              {item.title}
-            </Link>
-            {item.subMenu && openIndexes[index] && (
-              <ul className="sub_menu">
-                {item.subMenu.map((subItem, subIndex) => (
-                  <li key={subIndex}>
-                    <Link to={subItem.path}>{subItem.title}</Link>
-                  </li>
-                ))}
-              </ul>
-            )}
-          </li>
-        ))}
+        {menuItems.map((item, index) => {
+          const isActive = isMainMenuActive(item);
+
+          return (
+            <li key={index}>
+              <Link
+                to={item.path}
+                onClick={() => {
+                  if (item.subMenu) toggleSubMenu(index);
+                }}
+                style={{ color: isActive ? "#77c2ff" : "white" }}
+              >
+                {item.title}
+              </Link>
+              {item.subMenu && (
+                <ul
+                  className="sub_menu"
+                  style={{ display: openIndexes[index] ? "none" : "block" }}
+                >
+                  {item.subMenu.map((subItem, subIndex) => {
+                    const isSubActive = location.pathname === subItem.path;
+                    return (
+                      <li key={subIndex}>
+                        <Link
+                          to={subItem.path}
+                          style={{ color: isSubActive ? "#77c2ff" : "white" }}
+                        >
+                          {subItem.title}
+                        </Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              )}
+            </li>
+          );
+        })}
       </ul>
 
       <style jsx>{`
@@ -88,7 +118,6 @@ function SideMenu() {
               display: inline-block;
               height: 50px;
               line-height: 50px;
-              color: white;
               width: 100%;
               text-align: center;
             }
@@ -96,11 +125,6 @@ function SideMenu() {
           ul.main_menu {
             > li {
               background: var(--primary-600, #003181);
-              &:nth-of-type(1) {
-                a {
-                  color: #77c2ff;
-                }
-              }
               a {
                 font-size: 16px;
                 font-weight: 700;
