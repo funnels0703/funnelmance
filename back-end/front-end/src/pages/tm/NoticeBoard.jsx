e128a246aaa0526a44030c613ee27c38bc3194bc
funnelsJ <lj_yeon_@naver.com>
2024-09-23 00:04:40 +0900

    tm공지 수정

--- back-end/front-end/src/pages/tm/NoticeBoard.jsx
+++ back-end/front-end/src/pages/tm/NoticeBoard.jsx
@@ -1,5 +1,42 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+const dummyData = {
+  noticeNotices: [
+    {
+      id: 1,
+      title: "2024년 신년 인사 공지",
+      user: { username: "admin" },
+      created_at: "2024-01-01T09:00:00Z",
+    },
+    {
+      id: 2,
+      title: "시스템 점검 안내",
+      user: { username: "system" },
+      created_at: "2024-01-10T10:00:00Z",
+    },
+  ],
+  generalNotices: [
+    {
+      id: 3,
+      title: "회원가입 이벤트 안내",
+      user: { username: "user1" },
+      created_at: "2024-02-15T15:30:00Z",
+    },
+    {
+      id: 4,
+      title: "봄맞이 할인 행사 공지",
+      user: { username: "user2" },
+      created_at: "2024-03-10T14:20:00Z",
+    },
+    {
+      id: 5,
+      title: "서비스 개선 설문조사 참여 요청",
+      user: { username: "user3" },
+      created_at: "2024-03-20T11:45:00Z",
+    },
+  ],
+};
 
 const NoticeBoard = () => {
   const [noticeNotices, setNoticeNotices] = useState([]);
@@ -12,6 +49,7 @@ const NoticeBoard = () => {
     startDate: "",
     endDate: "",
   });
+  const navigate = useNavigate(); // navigate 함수 사용
 
   useEffect(() => {
     // 현재 연도와 월을 구해서 날짜를 설정
@@ -63,6 +101,12 @@ const NoticeBoard = () => {
     fetchNotices();
   }, [currentPage, filters]);
 
+  // useEffect(() => {
+  //   setNoticeNotices(dummyData.noticeNotices);
+  //   setGeneralNotices(dummyData.generalNotices);
+  //   setTotalPages(1); // 더미 데이터에 맞춰서 페이지 수 설정
+  // }, []);
+
   const handlePageChange = (pageNum) => {
     setCurrentPage(pageNum);
   };
@@ -106,80 +150,101 @@ const NoticeBoard = () => {
     setCurrentPage(1); // 검색 시 첫 페이지로 이동
   };
 
-  return (
-    <div>
-      <h1>공지사항</h1>
+  const handleRowClick = (id) => {
+    navigate(`/tm/notices/${id}`); // 게시글 ID를 포함한 경로로 이동
+  };
 
+  const handleNewClick = (id) => {
+    navigate(`/tm/notices/new`); // 게시글 ID를 포함한 경로로 이동
+  };
+
+  return (
+    <div className="notice_board_container">
       <form onSubmit={handleSearch}>
-        <input
-          type="text"
-          name="searchQuery"
-          value={filters.searchQuery}
-          onChange={handleFilterChange}
-          placeholder="Search..."
-        />
-        <div>
-          <label>
-            <input
-              type="checkbox"
-              name="title"
-              checked={filters.searchTypes.title}
-              onChange={handleFilterChange}
-            />
-            제목
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              name="author"
-              checked={filters.searchTypes.author}
-              onChange={handleFilterChange}
-            />
-            작성자
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              name="content"
-              checked={filters.searchTypes.content}
-              onChange={handleFilterChange}
-            />
-            내용
-          </label>
+        <div className="search_box">
+          <div className="search_type_box">
+            <div className="search_types">
+              <label>
+                <input
+                  type="checkbox"
+                  name="title"
+                  checked={filters.searchTypes.title}
+                  onChange={handleFilterChange}
+                />
+                제목
+              </label>
+              <label>
+                <input
+                  type="checkbox"
+                  name="author"
+                  checked={filters.searchTypes.author}
+                  onChange={handleFilterChange}
+                />
+                작성자
+              </label>
+              <label>
+                <input
+                  type="checkbox"
+                  name="content"
+                  checked={filters.searchTypes.content}
+                  onChange={handleFilterChange}
+                />
+                내용
+              </label>
+            </div>
+          </div>
+          <input
+            type="text"
+            name="searchQuery"
+            value={filters.searchQuery}
+            onChange={handleFilterChange}
+            placeholder="검색할 내용을 입력하세요."
+          />
         </div>
-        <input
-          type="month"
-          name="startDate"
-          value={filters.startDate}
-          onChange={handleFilterChange}
-        />
-        <input
-          type="month"
-          name="endDate"
-          value={filters.endDate}
-          onChange={handleFilterChange}
-        />
+        <div className="search_date">
+          날짜{` `}
+          <input
+            type="month"
+            name="startDate"
+            value={filters.startDate}
+            onChange={handleFilterChange}
+          />
+          {` `}~{` `}
+          <input
+            type="month"
+            name="endDate"
+            value={filters.endDate}
+            onChange={handleFilterChange}
+          />
+        </div>
         <button type="button" onClick={handleReset}>
-          Reset
+          검색 초기화
         </button>
+        <button onClick={handleNewClick}>+ 새글 작성</button>
       </form>
 
       <table>
         <thead>
           <tr>
             <th>No</th>
-            <th>Title</th>
-            <th>Author</th>
-            <th>Created At</th>
+            <th>제목</th>
+            <th>작성자</th>
+            <th>작성일</th>
           </tr>
         </thead>
         <tbody>
           {noticeNotices.length > 0 && (
             <>
               {noticeNotices.map((notice) => (
-                <tr key={notice.id}>
-                  <td>{notice.id}</td>
-                  <td>{notice.title}</td>
+                <tr
+                  key={notice.id}
+                  className="notice_tr"
+                  onClick={() => handleRowClick(notice.id)}
+                >
+                  <td>
+                    <img src="/images/notice/notice_icon.png" alt="공지사항" />
+                  </td>
+                  <td className="notice_title">{notice.title}</td>
                   <td>{notice.user.username}</td>
                   <td>{new Date(notice.created_at).toLocaleDateString()}</td>
                 </tr>
@@ -188,9 +253,13 @@ const NoticeBoard = () => {
           )}
           {generalNotices.length > 0 ? (
             generalNotices.map((notice) => (
-              <tr key={notice.id}>
+              <tr
+                key={notice.id}
+                className="general_tr"
+                onClick={() => handleRowClick(notice.id)}
+              >
                 <td>{notice.id}</td>
-                <td>{notice.title}</td>
+                <td className="notice_title">{notice.title}</td>
                 <td>{notice.user.username}</td>
                 <td>{new Date(notice.created_at).toLocaleDateString()}</td>
               </tr>
@@ -216,6 +285,104 @@ const NoticeBoard = () => {
           )
         )}
       </div>
+      <style jsx>
+        {`
+          .notice_board_container {
+            width: 100%;
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            form {
+              display: flex;
+              align-items: center;
+              height: 30px;
+              margin-bottom: 20px;
+              gap: 10px;
+              .search_box {
+                border: 1px solid #ccc;
+                border-radius: 5px;
+                height: 100%;
+                display: flex;
+                .search_type_box {
+                  border-right: 1px solid #ccc;
+                  padding: 0 8px;
+                  display: flex;
+                  align-items: center;
+                  .search_types {
+                    display: flex;
+                    gap: 10px;
+                    input {
+                      margin-right: 3px;
+                    }
+                  }
+                }
+                > input {
+                  border: none;
+                  padding: 0 8px;
+                }
+              }
+              .search_date {
+                height: 100%;
+                input {
+                  border: 1px solid #ccc;
+                  border-radius: 5px;
+                  height: 100%;
+                  padding: 0 8x;
+                }
+              }
+              button {
+                border-radius: 5px;
+                background: #4880ff;
+                color: #fff;
+                border: none;
+                font-size: 16px;
+                font-weight: 600;
+                padding: 5px 10px;
+              }
+            }
+            table {
+              width: 100%;
+              border-spacing: 0;
+              text-align: center;
+              font-size: 14px;
+              font-weight: 500;
+              color: #525252;
+              thead {
+                background-color: #fff;
+                box-shadow: 0 0 5px rgb(0 0 0 / 20%);
+                position: sticky;
+                top: 0;
+                z-index: 3;
+              }
+              tr {
+                width: 100%;
+                &.notice_tr {
+                  background-color: #fffcf3;
+                  font-weight: 600;
+                }
+                th {
+                  border-top: 1px solid #f5f5f5;
+                  border-bottom: 1px solid #f5f5f5;
+                  height: 45px;
+                  line-height: 45px;
+                }
+                td {
+                  height: 45px;
+                  line-height: 45px;
+                  border-bottom: 1px solid #f5f5f5;
+                  img {
+                    height: 60%;
+                    padding-top: 8px;
+                  }
+                  &.notice_title {
+                    text-align: left;
+                  }
+                }
+              }
+            }
+          }
+        `}
+      </style>
     </div>
   );
 };
